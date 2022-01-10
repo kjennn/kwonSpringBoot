50c168be4820e680fd3a87e32e905f9b8768f063
kjennn <jijo05037@naver.com>
2022-01-10 22:13:02 +0900

    component로 thymeLeaf 사용해 적용시켜보기

---
+++ src/main/java/com/kwon/ji/eun/KwonSpringboot/config/testConst.java
@@ -0,0 +1,9 @@
+package com.kwon.ji.eun.KwonSpringboot.config;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class testConst {
+    public String TEST_CODE = "1234";
+}
